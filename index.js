2259d88f500688870b399283a3dba8b5fbb1b388
ithilelda <ithilelda@gmail.com>
2024-06-18 11:08:40 +0000

    use commander to make cli.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const bilibili = require('./bilibili');
 const douyu = require('./douyu');
+const { Command } = require('commander');
 
+const program = new Command();
+program
+  .name("live_url")
+  .description("Get live url")
+  .version("0.1.0");
 
-module.exports = {
-  bilibili,
-  douyu,
-};
+program
+  .command("douyu")
+  .description("Get douyu live url")
+  .argument("<room_number>", "Room number of the live")
+  .action(async (room_number) => {
+    console.log(await douyu.get_h5_url(room_number));
+  });
 
-douyu.get_h5_url(88660).then(console.log);
+program
+  .command("bilibili")
+  .description("Get bilibili live url")
+  .argument("<room_number>", "Room number of the live")
+  .action(async (room_number) => {
+    console.log(await bilibili.get_live_urls(room_number));
+  });
+
+program.parse();
